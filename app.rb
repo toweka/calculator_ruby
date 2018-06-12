693531ddd6a29b5a98606508cee8f4e8ec32ced9
TECH-A58 <tech-a58@TECH-A58s-MacBook-Air.local>
2018-06-12 11:02:25 +0100

    laid out plans for calculator

---
+++ app.rb
@@ -0,0 +1,30 @@
+#Calculator
+
+#Prompt for basic or advanced
+
+#Basic Mode
+
+  #Prompt for function
+  #Prompt for num1
+  #Prompt for num2
+
+  #Prompt for function
+    #Addition
+      #Return result
+
+    #Subtraction
+      #Return result
+
+    #Multiply
+      #Return result
+
+    #Divide
+      #Return result
+
+#Advanced Mode
+
+    #Power
+      #Return result
+
+    #Square Root
+      #Return result
